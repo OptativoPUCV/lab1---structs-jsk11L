91f4e4f97a1098bdf00c0e0b9d4646288068f5ec
Replit user <>
2023-03-11 07:15:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -77,6 +77,7 @@ Vector * crearVector(int n) {
   vector1->datos = (int *) calloc(n+1, sizeof(int));
   vector1->capacidad = n;
   return vector1;
+  //Necesité ayuda de chat gpt en este ejercicio
 }
 
 /*
@@ -115,6 +116,9 @@ Ejercicio 8.
 Use las operaciones implementadas de vectores para 
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
-void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
 
+void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
+  c->datos[0] = a1+b1;
+  c->datos[1] = a2+b2;
+  return;
 }
