2016920b8cfb1eef7c6eeffd12441004ef379da9
Replit user <>
2023-03-11 07:09:48 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -85,9 +85,6 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-  if (i < 0 || i >= v->capacidad) {
-    return;
-  }
   v->datos[i] = valor;
   return;
 }
@@ -98,7 +95,7 @@ Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
-   return 0;
+   return v->datos[i];
 }
 
 /*
